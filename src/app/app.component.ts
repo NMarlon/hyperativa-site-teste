9de378c3a9acf64178c4794e922f58603c82319a
NMarlon <marlonrviana@gmail.com>
2021-05-03 17:55:29 -0300

    Primeira parte da página feita
    - Ainda falta arrumar o espaço depois do "!" no título
    - A imagem foi cortada manualmente
    - Ainda falta arrumar o título: "Hyperativa3"
    - Tornar o layout responsivo com @media only screen para celulares e tablets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import  *  as  data  from  './data.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
+  title = 'Hyperativa';
   public div: boolean = true;
+  
+  
 }
-<app-header></app-header>
